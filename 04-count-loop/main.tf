a01462dbdcd7f11e604ea254a5583fdd012da61a
Chandu2828 <purnachand7569@gmail.com>
2026-06-11 22:37:33 +0530

    added files

--- 04-count-loop/main.tf
+++ 04-count-loop/main.tf
@@ -1,15 +1,21 @@
-resource "aws_instance" "terraform_demo" {
-  count   = 10
+resource "aws_instance" "roboshop" {
+  count   = 4
   ami                    = var.ami_id
-  instance_type          = var.environment == "dev" ? "t3.micro" : "t3.small" # env = true then t3.micro, if env = false then t3.small
-  vpc_security_group_ids = [aws_security_group.allow_terraform.id] # list
-  # labels, metadata, info, etc
-  tags = var.ec2_tags
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [
+    aws_security_group.roboshop[count.index].id,
+    aws_security_group.common.id
+  ]
+  tags = {
+    Name = "${var.project}.${var.environment}-${var.instances[count.index]}" # interpolation
+  }
 }
 
+
 # It creates in default VPC
-resource "aws_security_group" "allow_terraform" {
-  name        = var.sg_name
+resource "aws_security_group" "roboshop" {
+  count = 4
+  name        = "${var.project}-${var.environment}-${var.instances[count.index]}"
   description = "Allow TLS inbound traffic and all outbound traffic"
 
   # outbound traffic
@@ -20,5 +26,25 @@ resource "aws_security_group" "allow_terraform" {
     cidr_blocks = var.cidr
   }
 
-  tags = var.sg_tags
+  tags = {
+    Name = "${var.project}-${var.environment}-${var.instances[count.index]}"
+  }
 }
+
+resource "aws_security_group" "common" {
+    name = "${var.project}-${var.environment}-common"
+    description = "Allow TLS inbound traffic and all outbound traffic"
+
+    #outbound traffic 
+    egress {
+        from_port = var.port
+        to_port = var.port
+        protocol = "-1" # all traffic
+        cidr_blocks = var.cidr
+    }
+
+    tags = {
+        Name = "${var.project}-${var.environment}-common"
+    }
+}
+
