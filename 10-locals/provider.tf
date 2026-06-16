ddc196452898b8c19142628867c36f26067fc18a
Chandu2828 <purnachand7569@gmail.com>
2026-06-16 10:11:04 +0530

    added files

---
+++ 10-locals/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws =   {
+            source  =   "hashicorp/aws"
+            version = "6.48.0"
+        }
+    }
+
+    backend "s3" {
+        bucket = "remote-state-practice-90s"
+        key = "locals-demo.tfstate"
+        region = "us-east-1"
+        encrypt = true
+        use_lockfile = true
+    }
+}
+
+provider "aws" {
+    region = "us-easst-1"
+}
